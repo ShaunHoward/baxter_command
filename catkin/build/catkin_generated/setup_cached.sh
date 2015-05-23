c9fecf52385f47940be3284991c61c208be5b4b1
ShaunHoward <smh150@case.edu>
2015-05-23 15:20:13 -0400

    Initialized baxter project

---
+++ catkin/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/baxter-dev/ros_workspace/catkin/build/test_results"
+export CMAKE_PREFIX_PATH="/home/baxter-dev/ros_workspace/catkin/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/baxter-dev/ros_workspace/catkin/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/baxter-dev/ros_workspace/catkin/devel/lib:/home/baxter-dev/ros_workspace/catkin/devel/lib/x86_64-linux-gnu:/home/baxter-dev/ros_workspace/cwru-ros-pkg-hydro-gamma/catkin/devel/lib/x86_64-linux-gnu:/home/baxter-dev/ros_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/baxter-dev/ros_workspace/cwru-ros-pkg-hydro-gamma/catkin/devel/lib:/home/baxter-dev/ros_ws/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/baxter-dev/ros_workspace/catkin/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/baxter-dev/ros_workspace/catkin/devel/lib/pkgconfig:/home/baxter-dev/ros_workspace/catkin/devel/lib/x86_64-linux-gnu/pkgconfig:/home/baxter-dev/ros_workspace/cwru-ros-pkg-hydro-gamma/catkin/devel/lib/x86_64-linux-gnu/pkgconfig:/home/baxter-dev/ros_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/baxter-dev/ros_workspace/cwru-ros-pkg-hydro-gamma/catkin/devel/lib/pkgconfig:/home/baxter-dev/ros_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/baxter-dev/ros_workspace/catkin/build"
+export PYTHONPATH="/home/baxter-dev/ros_workspace/catkin/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/baxter-dev/ros_workspace/catkin/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/baxter-dev/ros_workspace/catkin/src:$ROS_PACKAGE_PATH"
+export ROS_TEST_RESULTS_DIR="/home/baxter-dev/ros_workspace/catkin/build/test_results"
